b97c62f2fad0a7ad7cc9b043da0295354b1a3032
mjhuman <vinodhmjhuman@gmail.com>
2017-02-09 20:30:36 +0530

    Initial commit

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/mjhuman/dotfiles.git
+git clone https://github.com/vinodhmj/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
